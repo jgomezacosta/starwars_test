a54a7d74b25027abc608a62674e7fab43117561b
Jonatan Gomez Acosta <jgacosta@goiar.com>
2021-02-11 02:48:05 -0300

    SW3 -  Crear modelo en MySQL, y primer subida completa de Laravel

---
+++ starwars_test/database/migrations/2021_02_11_053843_vehicles.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Vehicles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Vehicles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->index();
+            $table->string('model')->index();
+            $table->string('vehicle_class');
+            $table->string('manufacturer');
+            $table->string('length');
+            $table->string('cost_in_credits');
+            $table->string('crew');
+            $table->string('passengers');
+            $table->string('max_atmosphering_speed');
+            $table->string('cargo_capacity');
+            $table->string('consumables');
+
+            $table->string('films');
+            $table->string('pilots');
+
+            $table->string('url');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Vehicles');
+    }
+}
